864e282d984b64ea66502a288d86f68156b57e54
mercybassey <udohmercy911@gmail.com>
2023-12-29 16:30:02 +0100

    Added exception handling

--- file.py
+++ file.py
@@ -1,11 +1,14 @@
 import yaml
 
 def read_and_write_file(input, output):
-    with open(f'{input}.yaml', 'r') as f:
-        data =  list(yaml.safe_load_all(f))
-    print(data)
-
-    with open(f'{output}.yaml', 'w') as file:
-        yaml.dump_all(data, file)
+    try:
+        with open(f'{input}.yaml', 'r') as f:
+            data =  list(yaml.safe_load_all(f))
+        with open(f'{output}.yaml', 'w') as file:
+            yaml.dump_all(data, file)
+    except (FileNotFoundError, yaml.YAMLError):
+        print(f"Error: No valid YAML or YAML file found for '{input}'.") 
     
 read_and_write_file('example', 'output')
+
+
